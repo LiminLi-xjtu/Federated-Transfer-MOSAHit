990c81f50ad5953d1290c0f6bc66d33447365918
LiminLi-xjtu <59948715+LiminLi-xjtu@users.noreply.github.com>
2026-08-20 21:29:56 +0800

    Add files via upload

---
+++ Data_preprocessing/extract_RNASeq_expression.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import numpy as np
+
+# 导入CSV安装包
+# import csv
+
+# flag = True
+for cancer in ['TCGA-ACC', 'TCGA-BLCA', 'TCGA-BRCA', 'TCGA-CESC', 'TCGA-CHOL', 'TCGA-COAD', 'TCGA-DLBC',
+               'TCGA-ESCA', 'TCGA-GBM', 'TCGA-HNSC', 'TCGA-KICH', 'TCGA-KIRC', 'TCGA-KIRP', 'TCGA-LAML',
+               'TCGA-LGG', 'TCGA-LIHC', 'TCGA-LUAD', 'TCGA-LUSC', 'TCGA-MESO', 'TCGA-OV', 'TCGA-PAAD',
+               'TCGA-PCPG', 'TCGA-PRAD', 'TCGA-READ', 'TCGA-SARC', 'TCGA-SKCM', 'TCGA-STAD', 'TCGA-TGCT',
+               'TCGA-THCA', 'TCGA-THYM', 'TCGA-UCEC', 'TCGA-UCS', 'TCGA-UVM']:
+    source_file_path = 'Origin_Data/RNASeq/' + cancer + '.htseq_counts.tsv.gz'
+    target_file_path = 'RNASeq_source_to_csv/' + cancer + '.csv'
+
+    import pandas as pd
+
+    df = pd.read_csv(source_file_path, delimiter='\t')
+    # print(df.shape)
+    # print(df.iloc[:, 1:])
+    # print(df.iloc[:, 1:].transpose())
+    # print(df.transpose())
+
+    # print(Ensembl_ID)
+    dataframe_tran = df.transpose()
+    dataframe_tran = dataframe_tran.reset_index()
+    print(dataframe_tran)
+    ID = dataframe_tran.iloc[0, :].tolist()
+    temp_dataframe = dataframe_tran.iloc[1:, :]
+    temp_dataframe.columns = ID
+    print(temp_dataframe)
+    temp_dataframe.to_csv(target_file_path, index=False, header=True)
